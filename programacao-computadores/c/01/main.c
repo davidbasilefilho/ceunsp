209fc7e85449c8dc19df657b3b0fa78ca65a4432
David Basile Filho <davidbasilefilho@gmail.com>
2025-03-18 20:14:08 -0300

    mais código e explicações

--- programacao-computadores/c/01/main.c
+++ programacao-computadores/c/01/main.c
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Função principal. Toda aplicação em C deve ter uma função main, caso não for
+// uma biblioteca.
+// argc é o número de argumentos passados para o programa
+// argv é um array (lista) de strings que contém os argumentos passados para o
+// programa.
+// Uma função é um pedaço de código reutilizável
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n"); // printf mostra texto na linha de comando
   printf("Shine On You, Crazy Diamond!\n"); // \n pula linha
@@ -14,6 +20,14 @@ int main(int argc, char *argv[]) {
   scanf("%s", name); // Lê o nome do usuário. %s é o formato para string
 
   printf("Hello, %s!\n", name); // Usando o nome do usuário
-  getchar();                    // Espera o usuário pressionar Enter
+
+  int a = 0; // cria variável de valor inteiro 32-bit
+  scanf("\nDigite um número: %d ",
+        &a); // Lê um número inteiro. Por quê usamos &? Porque scanf espera um
+             // endereço de memória, isto é, um ponteiro. no scanf anterior, o
+             // name já é um ponteiro, então não precisamos do &.
+  printf("Esse número ao quadrado é igual a %d\n", a * a);
+
+  getchar(); // Espera o usuário pressionar Enter
   return 0;
 }
