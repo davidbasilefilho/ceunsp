a8519959a4a37d25f78bfee06618a31ad5341220
David Basile Filho <davidbasilefilho@gmail.com>
2025-03-18 20:02:04 -0300

    git ignore

--- programacao-computadores/c/01/main.c
+++ programacao-computadores/c/01/main.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
-  printf("Shine On You, Crazy Diamond!");
+  printf("Shine On You, Crazy Diamond!\n");
+  printf("\nWhat is your name?\n");
+
+  char *name;
+  scanf("%s", name);
+
+  printf("Hello, %s!\n", name); // Use the name
   getchar();
   return 0;
 }
