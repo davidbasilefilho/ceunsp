33b5e42657607d6623daadce934aa9ec2ba26ea5
David Basile Filho <davidbasilefilho@gmail.com>
2025-04-10 19:51:00 -0300

    sla

---
+++ modelagem/05/06/ted.md
@@ -0,0 +1,11 @@
+---
+id: ted
+aliases: []
+tags: []
+---
+
+- experimentação
+- serviço ao cliente
+- ajuda ao próximo
+- empatia
+- entender o que o cliente precisa a partir de uma conversa
--- programacao-computadores/c/04/classificacao.c
+++ programacao-computadores/c/04/classificacao.c
@@ -1,18 +1,29 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   int64_t x;
-  printf("Digite um número inteiro: ");
-  scanf("%ld", &x);
+  while (true) {
+    printf("Digite um número inteiro: ");
+    scanf("%ld", &x);
 
-  if (x > 0) {
-    printf("O número %ld é positivo.\n", x);
-  } else if (x < 0) {
-    printf("O número %ld é negativo.\n", x);
-  } else {
-    printf("O número %ld é zero.\n", x);
+    if (x > 0) {
+      printf("O número %ld é positivo.\n", x);
+    } else if (x < 0) {
+      printf("O número %ld é negativo.\n", x);
+    } else {
+      printf("O número %ld é zero.\n", x);
+    }
+
+    printf("Deseja continuar? (s/n): ");
+    while (getchar() != '\n')
+      ;
+    if (getchar() == 'n') {
+      printf("Até logo");
+      break;
+    }
   }
 
   return EXIT_SUCCESS;
